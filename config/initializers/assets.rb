21c639cf6e2b6da359b35afd0551f8ca08e4e2c5
Wilson Kihumba <wizzy@Mambo-mbaya.>
2025-02-20 09:28:54 +0300

    lint issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,7 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.precompile << "bootstrap.min.js"
